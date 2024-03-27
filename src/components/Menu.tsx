4f6d0d326afd87341cae25325e01dd61bf3837e8
SungUk <didtjddnr1@gmail.com>
2024-03-27 22:15:06 +0900

    3-10 end

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,12 +3,13 @@ import { BiUserCircle } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 import { MdLogout, MdLogin } from "react-icons/md";
 import { useContext } from "react";
-
+import AuthContext from "context/AuthContext";
 import { getAuth, signOut } from "firebase/auth";
 import { app } from "firebaseApp";
+import { AiOutlineSearch } from "react-icons/ai";
+import { IoMdNotificationsOutline } from "react-icons/io";
 
 import { toast } from "react-toastify";
-import AuthContext from "context/AuthContext";
 
 export default function MenuList() {
   const { user } = useContext(AuthContext);
@@ -24,6 +25,14 @@ export default function MenuList() {
           <BiUserCircle />
           Profile
         </button>
+        <button type="button" onClick={() => navigate("/search")}>
+          <AiOutlineSearch />
+          Search
+        </button>
+        <button type="button" onClick={() => navigate("/notifications")}>
+          <IoMdNotificationsOutline />
+          Notification
+        </button>
         {user === null ? (
           <button type="button" onClick={() => navigate("/users/login")}>
             <MdLogin />
@@ -36,7 +45,8 @@ export default function MenuList() {
               const auth = getAuth(app);
               await signOut(auth);
               toast.success("로그아웃 되었습니다.");
-            }}>
+            }}
+          >
             <MdLogout />
             Logout
           </button>
